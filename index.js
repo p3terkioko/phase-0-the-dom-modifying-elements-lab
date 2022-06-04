aa9d3059566042df20b2966f5f86bf10c546bd95
Peter Mulu Kioko <peter.kioko@student.moringaschool.com>
2022-06-04 16:27:05 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code here!
+main.remove();
+let newHeader = document.createElement("h1");
+ newHeader.setAttribute("id","victory");
+ newHeader.innerHTML = "YOUR - NAME is the champion";
